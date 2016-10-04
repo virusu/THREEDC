3e2a4ea03fe686c0bee3a49663e82885ede9a508
Viorel Rusu <viorelrusu89@gmail.com>
2016-10-04 16:57:39 +0200

    elasticsearch + angular + 3dc.js example working

--- script.js
+++ script.js
@@ -26,6 +26,8 @@ var json_data;
 
   var groupByRepo;
 
+ elasticstuff();
+
 // initialization
   //getJSON call, draw meshes with data
    $.getJSON("jsons/scm-commits.json", function(data) {
@@ -38,6 +40,75 @@ var json_data;
 ///////////////
 // FUNCTIONS //
 ///////////////
+
+function elasticstuff() {
+  //elasticsearch and angular
+    // App module
+    //
+    // The app module will contain all of the components the app needs (directives,
+    // controllers, services, etc.). Since it will be using the components within
+    // the elasticsearch module, define it a dependency.
+    var ExampleApp = angular.module('ExampleApp', ['elasticsearch']);
+
+    // Service
+    //
+    // esFactory() creates a configured client instance. Turn that instance
+    // into a service so that it can be required by other parts of the application
+    ExampleApp.service('client', function (esFactory) {
+      return esFactory({
+        host: 'localhost:9200',
+        apiVersion: '2.3',
+        log: 'trace'
+      });
+    });
+
+    // Controller
+    //
+    // It requires the "client" service, and fetches information about the server,
+    // it adds either an error or info about the server to $scope.
+    //
+    // It also requires the esFactory to that it can check for a specific type of
+    // error which might come back from the client
+    ExampleApp.controller('ExampleController', function ($scope, client, esFactory) {
+
+      client.cluster.state({
+        metric: [
+          'cluster_name',
+          'nodes',
+          'master_node',
+          'version'
+        ]
+        })
+/*      client.search({
+        index:'commits-index',
+        body: {
+          query: {
+            match: {
+              _id: 'f2776c6c64b0a84df55edc1542dd6e67420d9076_git://git.opnfv.org/qtip'
+            }
+          }
+        }
+      })
+*/
+      .then(function (resp) {
+        $scope.clusterState = resp;
+        $scope.error = null;
+      })
+      .catch(function (err) {
+        $scope.clusterState = null;
+        $scope.error = err;
+
+        // if the err is a NoConnections error, then the client was not able to
+        // connect to elasticsearch. In that case, create a more detailed error
+        // message
+        if (err instanceof esFactory.errors.NoConnections) {
+          $scope.error = new Error('Unable to connect to elasticsearch. ' +
+            'Make sure that it is running and listening at http://localhost:9200');
+        }
+      });
+
+    });
+}
 
 function init () {
 
